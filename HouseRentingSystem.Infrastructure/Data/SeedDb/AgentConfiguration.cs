a743d31008b561fa62f0cb0249386d691dd0f390
konfucianeca <vladogg56@gmail.com>
2025-01-04 17:02:36 +0200

    Database seeded

---
+++ HouseRentingSystem.Infrastructure/Data/SeedDb/AgentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Infrastructure.Data.SeedDb
+{
+    internal class AgentConfiguration : IEntityTypeConfiguration<Agent>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Agent> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Agent[] {data.Agent});
+        }
+    }
+}
---
+++ HouseRentingSystem.Infrastructure/Data/SeedDb/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Infrastructure.Data.SeedDb
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Category[] {data.CottageCategory,data.SingleCategory,data.DuplexCategory});
+        }
+    }
+}
