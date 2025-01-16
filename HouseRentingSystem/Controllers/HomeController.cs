d8ebf8c57a80a53f3ef66eca806cec45532ac58d
konfucianeca <vladogg56@gmail.com>
2025-01-16 16:57:34 +0200

    MVC Structure Added

--- HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using HouseRentingSystem.Models;
+﻿using HouseRentingSystem.Core.Models.Home;
+using HouseRentingSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Linq.Expressions;
 
 namespace HouseRentingSystem.Controllers
 {
@@ -15,7 +17,9 @@ namespace HouseRentingSystem.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var model = new IndexViewModel();
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
