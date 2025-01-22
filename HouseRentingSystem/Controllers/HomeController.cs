0d7f063e0b7e4175cba4ee10cd5572fbd97e3f82
konfucianeca <vladogg56@gmail.com>
2025-01-22 15:55:31 +0200

    Home controller amended

--- HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using HouseRentingSystem.Core.Models.Home;
+﻿using HouseRentingSystem.Core.Contracts.House;
+using HouseRentingSystem.Core.Models.Home;
 using HouseRentingSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,15 +10,18 @@ namespace HouseRentingSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHouseService houseService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IHouseService _houseService)
         {
             _logger = logger;
+            houseService = _houseService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var model = new IndexViewModel();
+            var model = await houseService.LastThreeHouses();
 
             return View(model);
         }
