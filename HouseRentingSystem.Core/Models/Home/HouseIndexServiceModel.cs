7e744a940186b839db2a5c6bcf81685b7dd6473a
konfucianeca <vladogg56@gmail.com>
2025-01-22 15:57:14 +0200

    House service interfase and service added

--- HouseRentingSystem.Core/Models/Home/HouseIndexServiceModel.cs
+++ HouseRentingSystem.Core/Models/Home/HouseIndexServiceModel.cs
@@ -2,5 +2,8 @@
 {
     public class HouseIndexServiceModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string ImageUrl { get; set; }= string.Empty;
     }
 }
