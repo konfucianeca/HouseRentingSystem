f3cfe582a94e5a85473383bb2d20676063b51b06
konfucianeca <vladogg56@gmail.com>
2025-03-22 14:09:25 +0200

    All houses statictic implemented

---
+++ HouseRentingSystem/Controllers/StatisticApiController.cs
@@ -0,0 +1,26 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace HouseRentingSystem.Controllers
+{
+    [Route("api/statistic")]
+    [ApiController]
+    public class StatisticApiController : ControllerBase
+    {
+        private readonly IStatisticService statisticService;
+        public StatisticApiController(IStatisticService _statisticService)
+        {
+            statisticService = _statisticService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStatistic()
+        {
+            var result = await statisticService.TotalAsync();
+
+            return Ok(result);
+        }
+    }
+}
